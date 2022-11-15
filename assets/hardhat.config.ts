0da639a73b4fe59b35f35a0557cc918a2a2221b8
steven2308 <steven2308@outlook.com>
2022-11-15 14:31:28 -0500

    Updates solidity compiler to 0.8.16.

--- assets/hardhat.config.ts
+++ assets/hardhat.config.ts
@@ -8,7 +8,7 @@ import 'solidity-coverage';
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: '0.8.15',
+    version: '0.8.16',
     settings: {
       optimizer: {
         enabled: true,
